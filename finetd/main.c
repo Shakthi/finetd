29802610b546682bc240539f388a666ba7518c24
Shakthi Prasad Gs <shakthi.gs87@gmail.com>
2022-07-13 23:56:11 +0530

    Initial Commit

---
+++ finetd/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  finetd
+//
+//  Created by Shakthi Prasad G S on 13/07/22.
+//  Copyright © 2022 Shakthi Prasad G S. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
