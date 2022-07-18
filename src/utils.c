bd931d7f367d661437fa3dec332aac8325be7e29
Shakthi Prasad Gs <shakthi.gs87@gmail.com>
2022-07-18 22:12:40 +0530

    Added newline

--- src/utils.c
+++ src/utils.c
@@ -5,10 +5,11 @@
 //  Created by Shakthi Prasad G S on 18/07/22.
 //  Copyright © 2022 Shakthi Prasad G S. All rights reserved.
 //
-
+#include <string.h>
+#include <stdlib.h>
 #include <stdarg.h>
 #include "utils.h"
-#include <stdlib.h>
+
 
 extern int LOG_ENABLED;
 
@@ -18,10 +19,15 @@ int slogf (
     va_list args;
  
     va_start(args,__format);
+    char dest[strlen(__format)+1];
+    
+    strcpy(dest,__format);
+    strcat(dest, "\n");
+
  
     int return_status = 0;
     if(LOG_ENABLED)
-        return_status = vprintf( __format, args);
+        return_status = vprintf( dest, args);
     
  
     va_end(args);
